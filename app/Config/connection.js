4885751018e6e464fe160973747f60a52a3e12db
Sarbjit <singhsarbjit940@gmail.com>
2020-04-08 20:17:52 +0530

    add middleware and set sql connection

---
+++ app/Config/connection.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+function sqlConnection() {
+    var connectionPool  = mysql.createPool({
+        host : HOST,
+        user : USER,
+        // password : PASSWORD,
+        database: DATABASE
+    });
+    global.connectionPool = connectionPool;
+    connectionPool.on("enqueue", function(){console.log("connection  queued")});
+    connectionPool.on("dequeue", function(){console.log("connection  dequeued")});
+}
+
+
+global.queryExecutePromissified  = function(statement, parameters) {
+    return new Promise ((resolve, reject) => {
+        connectionPool.query(statement, parameters, function (err, rows) {
+            console.log(this.sql);
+            if(err){
+                console.error(err);
+                return reject(err);
+            } 
+            resolve(rows);
+        });
+    });
+}
+
+sqlConnection();
---
+++ app/Middleware/auth.js
@@ -0,0 +1,16 @@
+let authController = {};
+
+
+authController.apiAuth = function (req, res, next) {
+  return new Promise ((resolve, reject) => {
+    try {
+      let api_key = req.query.api_key;
+      if (!api_key || api_key != API_KEY) {
+        reject ({code: 40, errors: ['Invalid Request']});
+      }
+    } catch (error) {
+      console.log (error);
+      reject (error);
+    }
+  });
+};
