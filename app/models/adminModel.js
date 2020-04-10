6a43b83560bd8e7649f64fb78b03ddef412a7797
Sarbjit <singhsarbjit940@gmail.com>
2020-04-10 19:41:21 +0530

    user details

---
+++ app/models/adminModel.js
@@ -0,0 +1,16 @@
+const commonController = require('../Controllers/commonController')
+let adminModel = {};
+
+adminModel.userDetails = function(userDetail) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await commonController.insertInDb(userDetail, "users");
+      resolve({ message: ["User Successfully Inserted"], data: user });
+    } catch (error) {
+      console.log("error =========>", error);
+      return reject(error);
+    }
+  });
+};
+
+module.exports = adminModel;
